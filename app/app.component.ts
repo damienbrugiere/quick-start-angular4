5eae550c3c87ac0a0aa105ded992fa64c2f521a0
BRUGIERE DAMIEN <damien.brugiere.btssio@gmail.com>
2018-02-07 19:42:23 +0100

    initial project

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'component-sample',
+    template: `<h1>Hello, Angular 4 !</h1>`,
+})
+export class AppComponent { }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Quick start: Angular 4
+
+### Pour lancer l'application : 
+
+```
+npm start
+```
+
+### Generator Angular 4 
